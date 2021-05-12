c95f530326ac29d8a28a154232ec43c87a5c5da9
Alan Lai <alan.n.lai@gmail.com>
2021-05-11 22:38:43 -0700

    Update set-ssh-github.sh
    prevent dups of public keys

--- _scripts/set-ssh-github.sh
+++ _scripts/set-ssh-github.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-if [ $# -ne 1 ] 
+if [ $# -ne 1 ]
 then
   echo 1>&2 "Usage: $0 GITHUB_USERNAME"
   exit 3
 fi
 
-curl https://api.github.com/users/$1/keys | grep '"key"' | sed 's/^.*\(ssh-rsa .*\)\"$/\1/' >> ~/.ssh/authorized_keys
-chmod 0600 ~/.ssh/authorized_keys
+if [[ ! -f ~/.ssh ]]
+then
+  mkdir ~/.ssh -p
+fi
+
+# download the ssh keys
+curl https://api.github.com/users/$1/keys | grep '"key"' | sed 's/^.*\(ssh-rsa .*\)\"$/\1/' >> ~/.ssh/authorized_keys_tmp
+
+# if file has contents, then replace the authorized keys file
+if [ -s ~/.ssh/authorized_keys_tmp ]
+then
+  rm ~/.ssh/authorized_keys -f
+  mv ~/.ssh/authorized_keys_tmp ~/.ssh/authorized_keys
+  chmod 0600 ~/.ssh/authorized_keys
+  echo "authorized keys file updated"
+else
+  echo "no keys downloaded, keeping existing authorized keys file"
+fi
